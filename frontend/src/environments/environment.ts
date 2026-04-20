d3493b530eb7fc30bb5913d76b74f3ea2a470a7a
KaioCarv <kaiocarvalho558@gmail.com>
2026-04-20 00:04:40 -0300

    (Feat) Criando Front-end majorVersion

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,8 @@
+import { Capacitor } from '@capacitor/core';
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: Capacitor.isNativePlatform()
+    ? 'http://172.19.193.142:8080'
+    : 'http://localhost:8080',
 };
